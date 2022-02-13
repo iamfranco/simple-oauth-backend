dbc33fdfd7f2b31a3d2af27901e1facadd926358
iamfranco <kinfaifrancochan@gmail.com>
2022-02-13 12:38:18 +0000

    use global cors

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,8 @@ mongoose.connect(
 
 // Middleware
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:3000", credentials: true }))
+// app.use(cors({ origin: "http://localhost:3000", credentials: true }))
+app.use(cors())
 
 app.set("trust proxy", 1)
 
